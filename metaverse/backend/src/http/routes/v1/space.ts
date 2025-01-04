88e26cbb1e7dbe28f6b3508761c08a2e68c01fcc
ribhavsingla <ribhavsingla2166@gmail.com>
2025-01-04 15:19:18 +0530

    add space routes

--- metaverse/backend/src/http/routes/v1/space.ts
+++ metaverse/backend/src/http/routes/v1/space.ts
@@ -1,3 +1,118 @@
 import express from 'express'
+import client from '../../../db/index'
+import { userMiddleware } from '../../middleware/user'
+import { CreateSpaceSchema } from '../../types'
 
 export const spaceRouter = express.Router()
+
+spaceRouter.post('/', userMiddleware, async (req, res) => {
+    console.log('inside spacerouter');
+    const parsedData = CreateSpaceSchema.safeParse(req.body)
+    if (!parsedData.success) {
+        console.log(JSON.stringify(parsedData))
+        res.status(400).json({ message: "Validation failed" })
+        return
+    }
+
+    if (!parsedData.data.mapId) {
+        const space = await client.space.create({
+            data: {
+                name: parsedData.data.name,
+                width: parseInt(parsedData.data.dimensions.split("x")[0]),
+                height: parseInt(parsedData.data.dimensions.split("x")[1]),
+                creatorId: req.userId!
+            }
+        });
+        res.json({ spaceId: space.id })
+        return;
+    }
+
+    const map = await client.map.findFirst({
+        where: {
+            id: parsedData.data.mapId
+        }, select: {
+            mapElements: true,
+            width: true,
+            height: true
+        }
+    })
+    console.log("after")
+    if (!map) {
+        res.status(400).json({ message: "Map not found" })
+        return
+    }
+    console.log("map.mapElements.length")
+    console.log(map.mapElements.length)
+
+    let space = await client.$transaction(async () => {
+        const space = await client.space.create({
+            data: {
+                name: parsedData.data.name,
+                width: map.width,
+                height: map.height,
+                creatorId: req.userId!,
+            }
+        })
+
+        await client.spaceElements.createMany({
+            data: map.mapElements.map(e => ({
+                spaceId: space.id,
+                elementId: e.elementId,
+                x: e.x!,
+                y: e.y!
+            }))
+        })
+
+        return space;
+    })
+
+    console.log("space crated")
+    res.json({ spaceId: space.id })
+})
+
+spaceRouter.delete("/:spaceId", userMiddleware, async (req, res) => {
+    console.log("req.params.spaceId", req.params.spaceId)
+    const space = await client.space.findUnique({
+        where: {
+            id: req.params.spaceId
+        }, select: {
+            creatorId: true
+        }
+    })
+    if (!space) {
+        res.status(400).json({ message: "Space not found" })
+        return
+    }
+
+    if (space.creatorId !== req.userId) {
+        console.log("code should reach here")
+        res.status(403).json({ message: "Unauthorized" })
+        return
+    }
+
+    await client.space.delete({
+        where: {
+            id: req.params.spaceId
+        }
+    })
+    res.json({ message: "Space deleted" })
+})
+
+spaceRouter.get("/all", userMiddleware, async (req, res) => {
+    const spaces = await client.space.findMany({
+        where: {
+            creatorId: req.userId!
+        }
+    });
+
+    res.json({
+        spaces: spaces.map(s => ({
+            id: s.id,
+            name: s.name,
+            thumbnail: s.thumbnail,
+            dimensions: `${s.width}x${s.height}`,
+        }))
+    })
+})
+
+
--- metaverse/backend/src/http/types/index.ts
+++ metaverse/backend/src/http/types/index.ts
@@ -14,3 +14,27 @@ export const SigninSchema = z.object({
 export const UpdateMetadataSchema = z.object({
     avatarId: z.string()
 })
+
+export const CreateSpaceSchema = z.object({
+    name: z.string(),
+    dimensions: z.string().regex(/^[0-9]{1,4}x[0-9]{1,4}$/),
+    mapId: z.string().optional(),
+})
+
+export const DeleteElementSchema = z.object({
+    id: z.string(),
+})
+
+export const AddElementSchema = z.object({
+    spaceId: z.string(),
+    elementId: z.string(),
+    x: z.number(),
+    y: z.number(),
+})
+
+export const CreateElementSchema = z.object({
+    imageUrl: z.string(),
+    width: z.number(),
+    height: z.number(),
+    static: z.boolean(),
+})
